7d1f03c62a2b8e283d4e25cca80245cb35bb346a
PolenzaniPietro <polenzani.pietro@franchettisalviani.net>
2025-03-19 10:12:32 +0100

    giocofinito

--- giocoImpiccato/Program.cs
+++ giocoImpiccato/Program.cs
@@ -2,12 +2,14 @@
     " spero tu già conosca le regole, in caso contrario... IMPARA LE REGOLE");
 int tentativi = 0, indizi = 0;
 string FilePath = " ";
-string trattino;
 Random rdn = new Random();
 int casuale;
-string parolaCasuale = " ";
+string parolaCasuale = "";
 string lingua = " ";
-string categoria= " ";
+string categoria = " ";
+bool vittoria = false;
+string lettere_inserite = "";
+
 //selezione difficolta
 Console.WriteLine("seleziona la difficoltà");
 Console.WriteLine("facile: 10 tentativi, 3 indizi");
@@ -15,69 +17,51 @@ Console.WriteLine("medio: 5 tentativi, 2 indizi");
 Console.WriteLine("difficile: 3 tentativi, 1 indizio");
 Console.WriteLine("impossibile: 1 tentativo, 0 indizi");
 string difficolta = Console.ReadLine();
-if (difficolta == "facile")
+
+if (difficolta == "facile" || difficolta == "f")
 {
     tentativi = 10;
     indizi = 3;
 }
-else if (difficolta == "medio")
+else if (difficolta == "medio" || difficolta == "m")
 {
     tentativi = 5;
     indizi = 2;
 }
-else if (difficolta == "difficile")
+else if (difficolta == "difficile" || difficolta == "d")
 {
     tentativi = 3;
     indizi = 1;
 }
-else if (difficolta == "impossibile")
+else if (difficolta == "impossibile" || difficolta == "i")
 {
     tentativi = 1;
     indizi = 0;
 }
+
 //selezione lingua e tipologia partita
 Console.WriteLine("seleziona la lingua della tua partita, italiano(ita) o inglese(ing)");
 lingua = Console.ReadLine();
+
 if (lingua == "ita") //gioco in italiano
 {
     Console.WriteLine("scegli la categoria della tua partita: (v) videogiochi, (a) animali, (p) parti del corpo, (n) natura");
-     categoria = Console.ReadLine();
+    categoria = Console.ReadLine();
     if (categoria == "v")
     {
-
         FilePath = "videogiochi.txt";
-        string[] lines = File.ReadAllLines(FilePath); 
-        StreamReader reader = new StreamReader(FilePath);
-        lines = File.ReadAllLines(FilePath);
-        casuale = rdn.Next(lines.Length);
-        parolaCasuale = lines[casuale];
     }
     else if (categoria == "a")
     {
         FilePath = "animali_italiano.txt";
-        string[] lines = File.ReadAllLines(FilePath);
-        StreamReader reader = new StreamReader(FilePath);
-        lines = File.ReadAllLines(FilePath);
-        casuale = rdn.Next(lines.Length);
-        parolaCasuale = lines[casuale];
     }
     else if (categoria == "p")
     {
         FilePath = "parti_del_corpo_italiano.txt";
-        string[] lines = File.ReadAllLines(FilePath);
-        StreamReader reader = new StreamReader(FilePath);
-        lines = File.ReadAllLines(FilePath);
-        casuale = rdn.Next(lines.Length);
-        parolaCasuale = lines[casuale];
     }
     else if (categoria == "n")
     {
         FilePath = "natura_italiano.txt";
-        string[] lines = File.ReadAllLines(FilePath);
-        StreamReader reader = new StreamReader(FilePath);
-        lines = File.ReadAllLines(FilePath);
-        casuale = rdn.Next(lines.Length);
-        parolaCasuale = lines[casuale];
     }
 }
 else if (lingua == "ing") //gioco in inglese
@@ -87,72 +71,94 @@ else if (lingua == "ing") //gioco in inglese
     if (categoria == "v")
     {
         FilePath = "videogiochi.txt";
-        string[] lines = File.ReadAllLines(FilePath);
-        StreamReader reader = new StreamReader(FilePath);
-        lines = File.ReadAllLines(FilePath);
-        casuale = rdn.Next(lines.Length);
-        parolaCasuale = lines[casuale];
     }
     else if (categoria == "a")
     {
         FilePath = "animali_inglese.txt";
-        string[] lines = File.ReadAllLines(FilePath);
-        StreamReader reader = new StreamReader(FilePath);
-        lines = File.ReadAllLines(FilePath);
-        casuale = rdn.Next(lines.Length);
-        parolaCasuale = lines[casuale];
     }
     else if (categoria == "p")
     {
         FilePath = "parti_del_corpo_inglese.txt";
-        string[] lines = File.ReadAllLines(FilePath);
-        StreamReader reader = new StreamReader(FilePath);
-        lines = File.ReadAllLines(FilePath);
-        casuale = rdn.Next(lines.Length);
-        parolaCasuale = lines[casuale];
     }
     else if (categoria == "n")
     {
         FilePath = "natura_inglese.txt";
-        string[] lines = File.ReadAllLines(FilePath);
-        StreamReader reader = new StreamReader(FilePath);
-        lines = File.ReadAllLines(FilePath);
-        casuale = rdn.Next(lines.Length);
-        parolaCasuale = lines[casuale];
     }
 }
-trattino = new string('_', parolaCasuale.Length );
-Console.WriteLine(trattino + "(" + parolaCasuale.Length + ")");
-while (tentativi > 0)
+
+
+
+ string[] lines = File.ReadAllLines(FilePath);
+ casuale = rdn.Next(lines.Length);
+ parolaCasuale = lines[casuale].ToLower();
+
+
+char[] trattino = new char[parolaCasuale.Length];
+for (int i = 0; i < trattino.Length; i++)
 {
+    trattino[i] = '_';
+}
+Console.WriteLine("( " + parolaCasuale.Length + " )");
+trattino[0] = parolaCasuale[0];
+trattino[trattino.Length - 1] = parolaCasuale[parolaCasuale.Length - 1];
+
+while (tentativi > 0 && !vittoria)
+{
+    Console.WriteLine();
+    for (int i = 0; i < trattino.Length; i++)
+    {
+        Console.Write(trattino[i]);
+    }
+    Console.WriteLine();
     Console.WriteLine("tentativi rimanenti: " + tentativi);
+    Console.WriteLine("lettere inserite: " + lettere_inserite);
     Console.WriteLine();
     Console.WriteLine("inserisci lettera(l), suggerimento(s), inserisci parola(p)");
-    string scelta=Console.ReadLine();
+
+    string scelta = Console.ReadLine();
     if (scelta == "l")
     {
         Console.WriteLine("inserisci la lettera");
-        char inserimento = char.ToLower(Console.ReadKey().KeyChar);
-        for (int i = 0; i<parolaCasuale.Length; i++)
+        char inserimento = Console.ReadKey().KeyChar;
+        Console.WriteLine();  
+
+
+        bool letteraTrovata = false;
+        for (int j = 0; j < parolaCasuale.Length; j++)
         {
-            if(inserimento == parolaCasuale[i])
+            if (inserimento == parolaCasuale[j])
             {
-                trattino += inserimento;
+                trattino[j] = inserimento;
+                letteraTrovata = true;
             }
-            else
+        }
+
+        if (!letteraTrovata)
+        {
+            Console.WriteLine("inserimento non corretto");
+            tentativi--;
+        }
+        lettere_inserite += inserimento;
+
+        vittoria = true;
+        for (int i = 0; i < trattino.Length; i++)
+        {
+            if (trattino[i] == '_')
             {
-                Console.WriteLine("inserimento non corretto");
-                tentativi--;
+                vittoria = false;
+                break;
             }
         }
     }
-    else if( scelta == "p") 
+    else if (scelta == "p")
     {
-        Console.WriteLine("prova ad indovinare");
-        string parola=Console.ReadLine();
-        if (parola== parolaCasuale)
+        Console.WriteLine("prova ad indovinare la parola:");
+        string parola = Console.ReadLine();
+        if (parola == parolaCasuale)
         {
-            Console.WriteLine("HAI VINTO!!!");
+            vittoria = true;
+            Console.WriteLine("CONGRATULAZIONI HAI VINTO!!!!!!");
+            break;
         }
         else
         {
@@ -160,9 +166,22 @@ while (tentativi > 0)
             tentativi--;
         }
     }
-    else if( scelta == "s")
-    { 
-        
+    else if (scelta == "s")
+    {
+        if (indizi > 0)
+        {
+            int indice = rdn.Next(1, parolaCasuale.Length - 1);
+            trattino[indice] = parolaCasuale[indice];
+            indizi--;
+        }
+        else
+        {
+            Console.WriteLine("indizi esauriti");
+        }
     }
+}
 
+if (!vittoria)
+{
+    Console.WriteLine("non sei riuscito ad indovinare la parola, SCARSO");
 }
