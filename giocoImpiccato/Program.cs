8f9a72ada8b57914ceb54bf036529cd73c782ec9
PolenzaniPietro <polenzani.pietro@franchettisalviani.net>
2025-03-22 08:58:58 +0100

    gioco finito

--- giocoImpiccato/Program.cs
+++ giocoImpiccato/Program.cs
@@ -1,5 +1,6 @@
-﻿Console.WriteLine("benvenuto giocatore!! stai per giocare al gioco dell'impiccato,\n" +
-    " spero tu già conosca le regole, in caso contrario... IMPARA LE REGOLE");
+﻿Console.WriteLine("Benvenuto giocatore!! Stai per giocare al gioco dell'impiccato,\n" +
+            "Spero tu già conosca le regole, in caso contrario... IMPARA LE REGOLE");
+
 int tentativi = 0, indizi = 0;
 string FilePath = " ";
 Random rdn = new Random();
@@ -7,181 +8,200 @@ int casuale;
 string parolaCasuale = "";
 string lingua = " ";
 string categoria = " ";
-bool vittoria = false;
 string lettere_inserite = "";
+int paroleIndovinate = 0;
+bool proseguimento = true;
+string sceltaProseguimento = "";
 
-//selezione difficolta
-Console.WriteLine("seleziona la difficoltà");
-Console.WriteLine("facile: 10 tentativi, 3 indizi");
-Console.WriteLine("medio: 5 tentativi, 2 indizi");
-Console.WriteLine("difficile: 3 tentativi, 1 indizio");
-Console.WriteLine("impossibile: 1 tentativo, 0 indizi");
-string difficolta = Console.ReadLine();
-
-if (difficolta == "facile" || difficolta == "f")
+while (proseguimento==true)
 {
-    tentativi = 10;
-    indizi = 3;
-}
-else if (difficolta == "medio" || difficolta == "m")
-{
-    tentativi = 5;
-    indizi = 2;
-}
-else if (difficolta == "difficile" || difficolta == "d")
-{
-    tentativi = 3;
-    indizi = 1;
-}
-else if (difficolta == "impossibile" || difficolta == "i")
-{
-    tentativi = 1;
-    indizi = 0;
-}
+    // selezione difficoltà
+    Console.WriteLine("Seleziona la difficoltà");
+    Console.WriteLine("Facile: 10 tentativi, 3 indizi");
+    Console.WriteLine("Medio: 5 tentativi, 2 indizi");
+    Console.WriteLine("Difficile: 3 tentativi, 1 indizio");
+    Console.WriteLine("Impossibile: 1 tentativo, 0 indizi");
+    string difficolta = Console.ReadLine();
 
-//selezione lingua e tipologia partita
-Console.WriteLine("seleziona la lingua della tua partita, italiano(ita) o inglese(ing)");
-lingua = Console.ReadLine();
+    if (difficolta == "facile" || difficolta == "f")
+    {
+        tentativi = 10;
+        indizi = 3;
+    }
+    else if (difficolta == "medio" || difficolta == "m")
+    {
+        tentativi = 5;
+        indizi = 2;
+    }
+    else if (difficolta == "difficile" || difficolta == "d")
+    {
+        tentativi = 3;
+        indizi = 1;
+    }
+    else if (difficolta == "impossibile" || difficolta == "i")
+    {
+        tentativi = 1;
+        indizi = 0;
+    }
 
-if (lingua == "ita") //gioco in italiano
-{
-    Console.WriteLine("scegli la categoria della tua partita: (v) videogiochi, (a) animali, (p) parti del corpo, (n) natura");
-    categoria = Console.ReadLine();
-    if (categoria == "v")
-    {
-        FilePath = "videogiochi.txt";
-    }
-    else if (categoria == "a")
-    {
-        FilePath = "animali_italiano.txt";
-    }
-    else if (categoria == "p")
-    {
-        FilePath = "parti_del_corpo_italiano.txt";
-    }
-    else if (categoria == "n")
-    {
-        FilePath = "natura_italiano.txt";
-    }
-}
-else if (lingua == "ing") //gioco in inglese
-{
-    Console.WriteLine("select the category: (v) videogames, (a) animals, (p) body parts, (n) nature");
-    categoria = Console.ReadLine();
-    if (categoria == "v")
-    {
-        FilePath = "videogiochi.txt";
-    }
-    else if (categoria == "a")
-    {
-        FilePath = "animali_inglese.txt";
-    }
-    else if (categoria == "p")
-    {
-        FilePath = "parti_del_corpo_inglese.txt";
-    }
-    else if (categoria == "n")
-    {
-        FilePath = "natura_inglese.txt";
-    }
-}
+    // selezione lingua e tipologia partita
+    Console.WriteLine("Seleziona la lingua della tua partita, italiano(ita) o inglese(ing)");
+    lingua = Console.ReadLine();
 
+    if (lingua == "ita") // gioco in italiano
+    {
+        Console.WriteLine("Scegli la categoria della tua partita: (v) videogiochi, (a) animali, (p) parti del corpo, (n) natura");
+        categoria = Console.ReadLine();
+        if (categoria == "v")
+        {
+            FilePath = "videogiochi.txt";
+        }
+        else if (categoria == "a")
+        {
+            FilePath = "animali_italiano.txt";
+        }
+        else if (categoria == "p")
+        {
+            FilePath = "parti_del_corpo_italiano.txt";
+        }
+        else if (categoria == "n")
+        {
+            FilePath = "natura_italiano.txt";
+        }
+    }
+    else if (lingua == "ing") // gioco in inglese
+    {
+        Console.WriteLine("Select the category: (v) videogames, (a) animals, (p) body parts, (n) nature");
+        categoria = Console.ReadLine();
+        if (categoria == "v")
+        {
+            FilePath = "videogiochi.txt";
+        }
+        else if (categoria == "a")
+        {
+            FilePath = "animali_inglese.txt";
+        }
+        else if (categoria == "p")
+        {
+            FilePath = "parti_del_corpo_inglese.txt";
+        }
+        else if (categoria == "n")
+        {
+            FilePath = "natura_inglese.txt";
+        }
+    }
 
+    string[] lines = File.ReadAllLines(FilePath);
+    casuale = rdn.Next(lines.Length);
+    parolaCasuale = lines[casuale].ToLower();
 
- string[] lines = File.ReadAllLines(FilePath);
- casuale = rdn.Next(lines.Length);
- parolaCasuale = lines[casuale].ToLower();
-
-
-char[] trattino = new char[parolaCasuale.Length];
-for (int i = 0; i < trattino.Length; i++)
-{
-    trattino[i] = '_';
-}
-Console.WriteLine("( " + parolaCasuale.Length + " )");
-trattino[0] = parolaCasuale[0];
-trattino[trattino.Length - 1] = parolaCasuale[parolaCasuale.Length - 1];
-
-while (tentativi > 0 && !vittoria)
-{
-    Console.WriteLine();
+    char[] trattino = new char[parolaCasuale.Length];
     for (int i = 0; i < trattino.Length; i++)
     {
-        Console.Write(trattino[i]);
+        trattino[i] = '_';
     }
-    Console.WriteLine();
-    Console.WriteLine("tentativi rimanenti: " + tentativi);
-    Console.WriteLine("lettere inserite: " + lettere_inserite);
-    Console.WriteLine();
-    Console.WriteLine("inserisci lettera(l), suggerimento(s), inserisci parola(p)");
 
-    string scelta = Console.ReadLine();
-    if (scelta == "l")
+    Console.WriteLine("( " + parolaCasuale.Length + " )");
+    trattino[0] = parolaCasuale[0];
+    trattino[trattino.Length - 1] = parolaCasuale[parolaCasuale.Length - 1];
+
+    while (tentativi > 0)
     {
-        Console.WriteLine("inserisci la lettera");
-        char inserimento = Console.ReadKey().KeyChar;
-        Console.WriteLine();  
-
-
-        bool letteraTrovata = false;
-        for (int j = 0; j < parolaCasuale.Length; j++)
-        {
-            if (inserimento == parolaCasuale[j])
-            {
-                trattino[j] = inserimento;
-                letteraTrovata = true;
-            }
-        }
-
-        if (!letteraTrovata)
-        {
-            Console.WriteLine("inserimento non corretto");
-            tentativi--;
-        }
-        lettere_inserite += inserimento;
-
-        vittoria = true;
+        Console.WriteLine();
         for (int i = 0; i < trattino.Length; i++)
         {
-            if (trattino[i] == '_')
+            Console.Write(trattino[i]);
+        }
+        Console.WriteLine();
+        Console.WriteLine("Tentativi rimanenti: " + tentativi);
+        Console.WriteLine("Lettere inserite: " + lettere_inserite);
+        Console.WriteLine();
+        Console.WriteLine("Inserisci lettera(l), suggerimento(s), inserisci parola(p), termina gioco(t)");
+
+        string scelta = Console.ReadLine();
+        if (scelta == "l")
+        {
+            Console.WriteLine("Inserisci la lettera");
+            char inserimento = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            bool letteraTrovata = false;
+            for (int j = 0; j < parolaCasuale.Length; j++)
             {
-                vittoria = false;
+                if (inserimento == parolaCasuale[j])
+                {
+                    trattino[j] = inserimento;
+                    letteraTrovata = true;
+                }
+            }
+
+            if (!letteraTrovata)
+            {
+                Console.WriteLine("Inserimento non corretto");
+                tentativi--;
+            }
+            lettere_inserite += inserimento;
+        }
+        else if (scelta == "p")
+        {
+            Console.WriteLine("Prova ad indovinare la parola:");
+            string parolaIndovinata = Console.ReadLine();
+            if (parolaIndovinata == parolaCasuale)
+            {
+                paroleIndovinate++;
+                Console.WriteLine("Hai indovinato la parola!");
+                Console.WriteLine("Vuoi continuare a giocare? (si/no)");
+                sceltaProseguimento = Console.ReadLine().ToLower();
+                if (sceltaProseguimento == "no")
+                {
+                    proseguimento = false;
+                    break;
+                }
+                else
+                {
+                    break; 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Inserimento sbagliato");
+                tentativi--;
+            }
+        }
+        else if (scelta == "s")
+        {
+            if (indizi > 0)
+            {
+                int indice = rdn.Next(1, parolaCasuale.Length - 1);
+                trattino[indice] = parolaCasuale[indice];
+                indizi--;
+            }
+            else
+            {
+                Console.WriteLine("Indizi esauriti");
+            }
+        }
+        else if (scelta == "t")
+        {
+            break;
+        }
+
+        if (new string(trattino) == parolaCasuale)
+        {
+            paroleIndovinate++;
+            Console.WriteLine("Hai indovinato la parola!");
+            Console.WriteLine("Vuoi continuare a giocare? (si/no)");
+            sceltaProseguimento = Console.ReadLine().ToLower();
+            if (sceltaProseguimento == "no")
+            {
+                proseguimento = false;
                 break;
             }
         }
     }
-    else if (scelta == "p")
+    if (!proseguimento) 
     {
-        Console.WriteLine("prova ad indovinare la parola:");
-        string parola = Console.ReadLine();
-        if (parola == parolaCasuale)
-        {
-            vittoria = true;
-            Console.WriteLine("CONGRATULAZIONI HAI VINTO!!!!!!");
-            break;
-        }
-        else
-        {
-            Console.WriteLine("inserimento sbagliato");
-            tentativi--;
-        }
-    }
-    else if (scelta == "s")
-    {
-        if (indizi > 0)
-        {
-            int indice = rdn.Next(1, parolaCasuale.Length - 1);
-            trattino[indice] = parolaCasuale[indice];
-            indizi--;
-        }
-        else
-        {
-            Console.WriteLine("indizi esauriti");
-        }
+        Console.WriteLine("Gioco terminato, hai indovinato " + paroleIndovinate + " parole.");
     }
 }
-
-if (!vittoria)
-{
-    Console.WriteLine("non sei riuscito ad indovinare la parola, SCARSO");
-}
+    
